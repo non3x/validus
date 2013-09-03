7faff6ee296f72b34d5ffd21191de37bafbd515e
Zakharenko Mikhail <zakharenko.mikhail@gmail.com>
2013-09-04 00:29:53 +0200

    Closure rule implemented, in case you want some advanced custom logic on the fly

---
+++ src/Validus/Rules/Closure.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Rules;
+
+use \Validus\Exceptions as Exceptions;
+
+class Closure extends Base {
+
+    public function applyTo($targetValue) {
+        return (boolean) $this->invokeClosure($targetValue);
+    }
+
+    protected function invokeClosure($targetValue) {
+        if (is_callable($this->condition)) {
+            $reflection = new \ReflectionFunction($this->condition);
+            if (!$reflection->isClosure()) {
+                throw new Exceptions\NotCallable("Specified condition is not closure, can't proceed");
+            }
+            return $reflection->invokeArgs(array($targetValue));
+        }
+        return false;
+    }
+
+    public function setDefaultErrorMessage() {
+        $this->setErrorMessage("Condition for {$this->propertyName} is not met");
+    }
+
+}
