96fbc86312a9504b89b588e66f1347ac0782753a
Zakharenko Mikhail <zakharenko.mikhail@gmail.com>
2013-09-04 00:35:35 +0200

    Regular expressions rule

---
+++ src/Validus/Rules/Regexp.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Rules;
+
+class Regexp extends Base {
+
+    public function applyTo($targetValue) {
+        return preg_match($this->condition, $targetValue);
+    }
+
+    public function setDefaultErrorMessage() {
+        $this->setErrorMessage("{$this->propertyName}  doesn't match {$this->condition}");
+    }
+
+}
