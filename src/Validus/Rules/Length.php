d545272643b91200cdbb024c8c8ead7b09e339b5
Zakharenko Mikhail <zakharenko.mikhail@gmail.com>
2013-09-04 00:35:03 +0200

    Length related (max,min,equal to) rules added

---
+++ src/Validus/Rules/Length.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Rules;
+
+class Length extends Base {
+
+    public function applyTo($targetValue) {
+        $max = new MaxLength($this->condition);
+        $min = new MinLength($this->condition);
+        return ($max->applyTo($targetValue) & $min->applyTo($targetValue));
+    }
+
+    public function setDefaultErrorMessage() {
+        $this->setErrorMessage("{$this->propertyName} length must be equal to {$this->condition}");
+    }
+
+}
---
+++ src/Validus/Rules/Minlength.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Rules;
+
+class Minlength extends Base {
+
+    public function applyTo($targetValue) {
+        return (strlen($targetValue) >= $this->condition);
+    }
+
+    public function setDefaultErrorMessage() {
+        $this->setErrorMessage("{$this->propertyName} minimum length is {$this->condition}");
+    }
+
+}
