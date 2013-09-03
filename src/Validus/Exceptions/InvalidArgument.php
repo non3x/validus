d2ebbd676174812fd0370bcd4f891b613a4c26bc
Zakharenko Mikhail <zakharenko.mikhail@gmail.com>
2013-09-04 00:25:41 +0200

    Validus Exceptions: invalid argument, not callable, nonexistent rule

---
+++ src/Validus/Exceptions/InvalidArgument.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Exceptions;
+
+class InvalidArgument extends \Exception {
+    // pass
+}
