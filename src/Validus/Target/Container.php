161f2863b586883e0977945dcbaee78bcf178d67
Zakharenko Mikhail <zakharenko.mikhail@gmail.com>
2013-09-04 00:03:04 +0200

    Abstract Container for uniform properties access

---
+++ src/Validus/Target/Container.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Target;
+
+abstract class Container {
+
+    /** @var type Reference to real target object, e.g $user model */
+    protected $target;
+
+    public function __construct($target) {
+        $this->target = $target;
+    }
+
+    /**
+     * Access target's properties by name
+     * 
+     * @param string $name property name
+     * @return mixed
+     */
+    abstract public function getValueByName($name);
+
+    /**
+     * @return array Of properties names or array keys
+     */
+    abstract public function getPropertiesNames();
+}
