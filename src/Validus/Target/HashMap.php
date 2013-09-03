91e0ed16e9dafb48f2ed571cafbf045c4abaa62a
Zakharenko Mikhail <zakharenko.mikhail@gmail.com>
2013-09-04 00:08:36 +0200

    Associative array target wrapper

---
+++ src/Validus/Target/HashMap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author    Zakharenko Mikhail 
+ * @copyright    Copyright 2013, Zakharenko Mikhail
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace Validus\Target;
+
+class HashMap extends Container {
+
+    public function getValueByName($name) {
+        if (!empty($this->target[$name])) {
+            return $this->target[$name];
+        }
+        return false;
+    }
+
+    public function getPropertiesNames() {
+        return array_keys($this->target);
+    }
+
+}
